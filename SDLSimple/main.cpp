6132b98e2206964f6c696f1f1e4a1690bab8a0a5
Mearaj Ahmed <mma618@nyu.edu>
2024-06-15 19:38:12 -0400

    final

--- SDLSimple/main.cpp
+++ SDLSimple/main.cpp
@@ -14,27 +14,6 @@
 1. [**Common Issues**](#common-issues)
 2. [**Extra Credit**](#extra-credit)
 
-### Requirement 1: _At Least Two Objects (50%)_
-
-- There needs to be at least 2 different "objects" in the scene.
-- Each object must use a different texture. These textures must _not_ be solid colours, but images. You may use any images that you like.
-
-### Requirement 2: _Movement (25%)_
-
-- At least one of your objects must be **moving in relation to other objects** (i.e. translation).
-
-### Requirement 3: _Rotation (25%)_
-
-- At least one of your objects must be **rotating**.
-
-### Common Issues
-
-If your screen is flickering, remember that you have to, _in order_:
-
-1. Call `glClear`.
-2. Draw every single one of your objects (i.e. everything you want to be in your scene)
-3. Call `SDL_GL_SwapWindow`.
-
 ### Extra Credit
 
 - Have at least one of your objects change scale.
@@ -92,18 +71,27 @@ LEVEL_OF_DETAIL = 0,
 TEXTURE_BORDER = 0;
 
 // Path to Sprites
-constexpr char PENGUIN_BLUE_FILEPATH[] = "assets/penguin_blue",
-PUFFLE_ORANGE_FILEPATH[] = "assets/puffle_orange";
-
-// Initial Scale of Sprites
-constexpr glm::vec3 INIT_SCALE = glm::vec3(2.5f, 5.263f, 0.0f),
+constexpr char PENGUIN_BLUE_FILEPATH[] = "assets/penguin_blue.png",
+PUFFLE_ORANGE_FILEPATH[] = "assets/puffle_orange.png";
 
 // Initial Position of Sprites
-INIT_POS_PENGUIN_BLUE = glm::vec3(0.0f, 0.0f, 0.0f),
-INIT_POS_PUFFLE_ORANGE = glm::vec3(0.0f, 0.0f, 0.0f);
+constexpr glm::vec3 INIT_POS_PENGUIN = glm::vec3(0.0f, 0.0f, 0.0f),
+INIT_POS_PUFFLE = glm::vec3(0.5f, -0.25f, 0.0f);
 
-// Rotational Increment
-constexpr float ROT_INCREMENT = 1.0f;
+constexpr glm::vec3 INIT_ROT_PENGUIN = glm::vec3(0.0f, 0.0f, 0.0f),
+INIT_ROT_PUFFLE = glm::vec3(0.0f, 0.0f, 0.0f);
+
+// Initial Scale of Sprites
+constexpr glm::vec3 INIT_SCALE_PENGUIN = glm::vec3(1.0f, 1.0f, 1.0f),
+INIT_SCALE_PUFFLE = glm::vec3(0.5f, 0.5, 0.5f);
+
+constexpr float PENGUIN_TRANSLATION_INCREMENT = 0.1f,
+PENGUIN_ROTATION_INCREMENT = 0.0f,
+PENGUIN_SCALE_INCREMENT = 0.1f;
+
+constexpr float PUFFLE_TRANSLATION_INCREMENT = 0.0f,
+PUFFLE_ROTATION_INCREMENT = 0.3f,
+PUFFLE_SCALE_INCREMENT = -0.05f;
 
 SDL_Window* g_display_window;
 AppStatus g_app_status = RUNNING;
@@ -116,181 +104,31 @@ g_projection_matrix;
 
 float g_previous_ticks = 0.0f;
 
-glm::vec3 g_rotation_penguin = glm::vec3(0.0f, 0.0f, 0.0f),
-g_rotation_puffle = glm::vec3(0.0f, 0.0f, 0.0f);
+glm::vec3 g_penguin_translation = glm::vec3(0.0f, 0.0f, 0.0f),
+g_puffle_translation = glm::vec3(0.0f, 0.0f, 0.0f);
+
+glm::vec3 g_penguin_rotation = glm::vec3(0.0f, 0.0f, 0.0f),
+g_puffle_rotation = glm::vec3(0.0f, 0.0f, 0.0f);
+
+glm::vec3 g_penguin_scale = glm::vec3(0.0f, 0.0f, 0.0f),
+g_puffle_scale = glm::vec3(0.0f, 0.0f, 0.0f);
 
 GLuint g_penguin_texture_id,
 g_puffle_texture_id;
 
+GLuint load_texture(const char* filepath);
 
-gluint load_texture(const char* filepath)
-{
-    // step 1: loading the image file
-    int width, height, number_of_components;
-    unsigned char* image = stbi_load(filepath, &width, &height, &number_of_components, stbi_rgb_alpha);
+void initialise();
 
-    //if (image == null)
-    //{
-    //    log("unable to load image. make sure the path is correct.");
-    //    assert(false);
-    //}
+void process_input();
 
-    //// step 2: generating and binding a texture id to our image
-    //gluint textureid;
-    //glgentextures(number_of_textures, &textureid);
-    //glbindtexture(gl_texture_2d, textureid);
-    //glteximage2d(gl_texture_2d, level_of_detail, gl_rgba, width, height, texture_border, gl_rgba, gl_unsigned_byte, image);
+void update();
+    
+void draw_object(glm::mat4& object_g_model_matrix, GLuint& object_texture_id);
 
-    //// step 3: setting our texture filter parameters
-    //gltexparameteri(gl_texture_2d, gl_texture_min_filter, gl_nearest);
-    //gltexparameteri(gl_texture_2d, gl_texture_mag_filter, gl_nearest);
+void render();
 
-    //// step 4: releasing our file from memory and returning our texture id
-    //stbi_image_free(image);
-
-    //return textureid;
-}
-
-
-void initialise()
-{
-    // Initialise video and joystick subsystems
-    SDL_Init(SDL_INIT_VIDEO);
-
-    g_display_window = SDL_CreateWindow("Hello, Textures!",
-        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-        WINDOW_WIDTH, WINDOW_HEIGHT,
-        SDL_WINDOW_OPENGL);
-
-    SDL_GLContext context = SDL_GL_CreateContext(g_display_window);
-    SDL_GL_MakeCurrent(g_display_window, context);
-
-    if (g_display_window == nullptr)
-    {
-        std::cerr << "Error: SDL window could not be created.\n";
-        SDL_Quit();
-        exit(1);
-    }
-
-#ifdef _WINDOWS
-    glewInit();
-#endif
-
-    glViewport(VIEWPORT_X, VIEWPORT_Y, VIEWPORT_WIDTH, VIEWPORT_HEIGHT);
-
-    g_shader_program.load(V_SHADER_PATH, F_SHADER_PATH);
-
-    //g_kano_matrix = glm::mat4(1.0f);
-    //g_mahiru_matrix = glm::mat4(1.0f);
-    //g_view_matrix = glm::mat4(1.0f);
-    //g_projection_matrix = glm::ortho(-5.0f, 5.0f, -3.75f, 3.75f, -1.0f, 1.0f);
-
-    //g_shader_program.set_projection_matrix(g_projection_matrix);
-    //g_shader_program.set_view_matrix(g_view_matrix);
-
-    //glUseProgram(g_shader_program.get_program_id());
-
-    glClearColor(BG_RED, BG_GREEN, BG_BLUE, BG_OPACITY);
-
-    //g_kano_texture_id = load_texture(KANO_SPRITE_FILEPATH);
-    //g_mahiru_texture_id = load_texture(MAHIRU_SPRITE_FILEPATH);
-
-    //glEnable(GL_BLEND);
-    //glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-}
-
-
-void process_input()
-{
-    SDL_Event event;
-    while (SDL_PollEvent(&event))
-    {
-        if (event.type == SDL_QUIT || event.type == SDL_WINDOWEVENT_CLOSE)
-        {
-            g_app_status = TERMINATED;
-        }
-    }
-}
-
-
-void update()
-{
-    /* Delta time calculations */
-    float ticks = (float)SDL_GetTicks() / MILLISECONDS_IN_SECOND;
-    float delta_time = ticks - g_previous_ticks;
-    g_previous_ticks = ticks;
-
-    ///* Game logic */
-    //g_rotation_kano.y += ROT_INCREMENT * delta_time;
-    //g_rotation_mahiru.y += -1 * ROT_INCREMENT * delta_time;
-
-    ///* Model matrix reset */
-    //g_kano_matrix = glm::mat4(1.0f);
-    //g_mahiru_matrix = glm::mat4(1.0f);
-
-    ///* Transformations */
-    //g_kano_matrix = glm::translate(g_kano_matrix, INIT_POS_KANO);
-    //g_kano_matrix = glm::rotate(g_kano_matrix,
-    //    g_rotation_kano.y,
-    //    glm::vec3(0.0f, 1.0f, 0.0f));
-    //g_kano_matrix = glm::scale(g_kano_matrix, INIT_SCALE);
-
-    //g_mahiru_matrix = glm::translate(g_mahiru_matrix, INIT_POS_MAHIRU);
-    //g_mahiru_matrix = glm::rotate(g_mahiru_matrix,
-    //    g_rotation_mahiru.y,
-    //    glm::vec3(0.0f, 1.0f, 0.0f));
-    //g_mahiru_matrix = glm::scale(g_mahiru_matrix, INIT_SCALE);
-}
-
-
-void draw_object(glm::mat4& object_g_model_matrix, GLuint& object_texture_id)
-{
-    //g_shader_program.set_model_matrix(object_g_model_matrix);
-    //glBindTexture(GL_TEXTURE_2D, object_texture_id);
-    //glDrawArrays(GL_TRIANGLES, 0, 6); // we are now drawing 2 triangles, so use 6, not 3
-}
-
-
-void render()
-{
-    glClear(GL_COLOR_BUFFER_BIT);
-
-    //// Vertices
-    //float vertices[] =
-    //{
-    //    -0.5f, -0.5f, 0.5f, -0.5f, 0.5f, 0.5f,  // triangle 1
-    //    -0.5f, -0.5f, 0.5f, 0.5f, -0.5f, 0.5f   // triangle 2
-    //};
-
-    //// Textures
-    //float texture_coordinates[] =
-    //{
-    //    0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f,     // triangle 1
-    //    0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 0.0f,     // triangle 2
-    //};
-
-    //glVertexAttribPointer(g_shader_program.get_position_attribute(), 2, GL_FLOAT, false,
-    //    0, vertices);
-    //glEnableVertexAttribArray(g_shader_program.get_position_attribute());
-
-    //glVertexAttribPointer(g_shader_program.get_tex_coordinate_attribute(), 2, GL_FLOAT,
-    //    false, 0, texture_coordinates);
-    //glEnableVertexAttribArray(g_shader_program.get_tex_coordinate_attribute());
-
-    //// Bind texture
-    //draw_object(g_kano_matrix, g_kano_texture_id);
-    //draw_object(g_mahiru_matrix, g_mahiru_texture_id);
-
-    //// We disable two attribute arrays now
-    //glDisableVertexAttribArray(g_shader_program.get_position_attribute());
-    //glDisableVertexAttribArray(g_shader_program.get_tex_coordinate_attribute());
-
-    SDL_GL_SwapWindow(g_display_window);
-}
-
-
-void shutdown() { SDL_Quit(); }
-
+void shutdown();
 
 int main(int argc, char* argv[])
 {
@@ -305,4 +143,157 @@ int main(int argc, char* argv[])
 
     shutdown();
     return 0;
+}
+
+void initialise()
+{
+    // Initialise video and joystick subsystems
+    SDL_Init(SDL_INIT_VIDEO);
+
+    g_display_window = SDL_CreateWindow("CLUB PENGUIN ?",
+        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+        WINDOW_WIDTH, WINDOW_HEIGHT,
+        SDL_WINDOW_OPENGL);
+
+    SDL_GLContext context = SDL_GL_CreateContext(g_display_window);
+    SDL_GL_MakeCurrent(g_display_window, context);
+
+    if (g_display_window == nullptr) {
+        std::cerr << "Error: SDL window could not be created.\n";
+        SDL_Quit();
+        exit(1);
+    }
+
+#ifdef _WINDOWS
+    glewInit();
+#endif
+
+    glViewport(VIEWPORT_X, VIEWPORT_Y, VIEWPORT_WIDTH, VIEWPORT_HEIGHT);
+
+    g_shader_program.load(V_SHADER_PATH, F_SHADER_PATH);
+
+    g_penguin_matrix = glm::mat4(1.0f);
+    g_puffle_matrix = glm::mat4(1.0f);
+    g_view_matrix = glm::mat4(1.0f);
+    g_projection_matrix = glm::ortho(-5.0f, 5.0f, -3.75f, 3.75f, -1.0f, 1.0f);
+
+    g_shader_program.set_projection_matrix(g_projection_matrix);
+    g_shader_program.set_view_matrix(g_view_matrix);
+
+    glUseProgram(g_shader_program.get_program_id());
+
+    glClearColor(BG_RED, BG_GREEN, BG_BLUE, BG_OPACITY);
+
+    g_penguin_texture_id = load_texture(PENGUIN_BLUE_FILEPATH);
+    g_puffle_texture_id = load_texture(PUFFLE_ORANGE_FILEPATH);
+
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+}
+
+void process_input()
+{
+    SDL_Event event;
+    while (SDL_PollEvent(&event))
+    {
+        if (event.type == SDL_QUIT || event.type == SDL_WINDOWEVENT_CLOSE)
+        {
+            g_app_status = TERMINATED;
+        }
+    }
+}
+
+void update()
+{
+    /* Delta time calculations */
+    float ticks = (float)SDL_GetTicks() / MILLISECONDS_IN_SECOND;
+    float delta_time = ticks - g_previous_ticks;
+    g_previous_ticks = ticks;
+
+    /* Game logic */
+    g_penguin_translation.x += PENGUIN_TRANSLATION_INCREMENT * delta_time;
+    g_penguin_translation.y += PENGUIN_TRANSLATION_INCREMENT * delta_time;
+
+    g_puffle_rotation.y += PUFFLE_ROTATION_INCREMENT * delta_time;
+
+    g_penguin_scale += PENGUIN_SCALE_INCREMENT * delta_time;
+    g_puffle_scale += PUFFLE_SCALE_INCREMENT * delta_time;
+
+    g_penguin_matrix = glm::mat4(1.0f);
+    g_puffle_matrix = glm::mat4(1.0f);
+
+    g_penguin_matrix = glm::translate(g_penguin_matrix, INIT_POS_PENGUIN + g_penguin_translation);
+    g_penguin_matrix = glm::rotate(g_penguin_matrix, g_penguin_rotation.y, glm::vec3(1.0f,0.0f,0.0f));
+    g_penguin_matrix = glm::scale(g_penguin_matrix, INIT_SCALE_PENGUIN + g_penguin_scale);
+
+    g_puffle_matrix = glm::translate(g_penguin_matrix, INIT_POS_PUFFLE + g_puffle_translation);
+    g_puffle_matrix = glm::rotate(g_puffle_matrix, g_puffle_rotation.y, glm::vec3(0.0f,1.0f,0.0f));
+    g_puffle_matrix = glm::scale(g_puffle_matrix, INIT_SCALE_PUFFLE + g_puffle_scale);
+
+}
+
+void draw_object(glm::mat4& object_g_model_matrix, GLuint& object_texture_id)
+{
+    g_shader_program.set_model_matrix(object_g_model_matrix);
+    glBindTexture(GL_TEXTURE_2D, object_texture_id);
+    glDrawArrays(GL_TRIANGLES, 0, 6); // we are now drawing 2 triangles, so use 6, not 3
+}
+
+void render()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    // Vertices
+    float vertices[] =
+    {
+        -0.5f, -0.5f, 0.5f, -0.5f, 0.5f, 0.5f,  // triangle 1
+        -0.5f, -0.5f, 0.5f, 0.5f, -0.5f, 0.5f   // triangle 2
+    };
+
+    // Textures
+    float texture_coordinates[] =
+    {
+        0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f,     // triangle 1
+        0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 0.0f,     // triangle 2
+    };
+
+    glVertexAttribPointer(g_shader_program.get_position_attribute(), 2, GL_FLOAT, false,
+        0, vertices);
+    glEnableVertexAttribArray(g_shader_program.get_position_attribute());
+
+    glVertexAttribPointer(g_shader_program.get_tex_coordinate_attribute(), 2, GL_FLOAT,
+        false, 0, texture_coordinates);
+    glEnableVertexAttribArray(g_shader_program.get_tex_coordinate_attribute());
+
+    // Bind texture
+    draw_object(g_penguin_matrix, g_penguin_texture_id);
+    draw_object(g_puffle_matrix, g_puffle_texture_id);
+
+    // We disable two attribute arrays now
+    glDisableVertexAttribArray(g_shader_program.get_position_attribute());
+    glDisableVertexAttribArray(g_shader_program.get_tex_coordinate_attribute());
+
+    SDL_GL_SwapWindow(g_display_window);
+}
+
+void shutdown() { SDL_Quit(); }
+
+GLuint load_texture(const char* filepath) {
+    // step 1: loading the image file
+    int width, height, number_of_components;
+    unsigned char* image = stbi_load(filepath, &width, &height, &number_of_components, STBI_rgb_alpha);
+
+    // step 2: generating and binding a texture id to our image
+    GLuint textureid;
+    glGenTextures(1, &textureid);
+    glBindTexture(GL_TEXTURE_2D, textureid);
+    glTexImage2D(GL_TEXTURE_2D, LEVEL_OF_DETAIL, GL_RGBA, width, height, TEXTURE_BORDER, GL_RGBA, GL_UNSIGNED_BYTE, image);
+
+    // step 3: setting our texture filter parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+
+    // step 4: releasing our file from memory and returning our texture id
+    stbi_image_free(image);
+
+    return textureid;
 }
